d078fa5e4bef90b2da3231a1a2456e6d95476041
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-09-01 00:28:05 +0300

    create pydantic settings for the app

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+from config import settings
 from views.calc import router as router_calc
 
 app = FastAPI()
 app.include_router(router_calc, prefix="/calc")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors.allow_origins,
+    allow_methods=settings.cors.allow_methods,
+    allow_headers=settings.cors.allow_headers,
+    allow_credentials=settings.cors.allow_credentials,
+)
 
 
 @app.get("/hello")
@@ -24,7 +33,10 @@ def hi_by_path(name: str = "World"):
 
 
 if __name__ == "__main__":
+    # print(settings.model_dump())
     uvicorn.run(
         "main:app",
-        reload=True,
+        host=settings.host,
+        port=settings.port,
+        reload=settings.reload,
     )
